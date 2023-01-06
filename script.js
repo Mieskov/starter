592770c2fd3d0e60e55bc63de3ea1f90a8b28ebb
Mieskov <mieskove@gmail.com>
2023-01-06 14:25:10 +0200

    com

--- script.js
+++ script.js
@@ -47,7 +47,8 @@ const measureKelvin = function () {
   const measurement = {
     type: "temp",
     unit: "celsius",
-    value: Number(prompt("Degrees celsius")),
+    value: 10,
+    //value: Number(prompt("Degrees celsius")),
   };
 
   const kelvin = measurement.value + 273;
@@ -55,3 +56,24 @@ const measureKelvin = function () {
 };
 
 console.log(measureKelvin());
+//
+
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([7, 9, 13], [-1, 4, 1]);
+console.log(amplitudeBug);
