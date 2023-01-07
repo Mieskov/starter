7a21ba585d2b0a8e853d0ca11cee726067948a26
Mieskov <mieskove@gmail.com>
2023-01-07 13:12:16 +0200

    com

--- script.js
+++ script.js
@@ -77,3 +77,16 @@ const calcTempAmplitudeBug = function (t1, t2) {
 
 const amplitudeBug = calcTempAmplitudeBug([7, 9, 13], [-1, 4, 1]);
 console.log(amplitudeBug);
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log("..." + str);
+};
+printForecast(data2);
